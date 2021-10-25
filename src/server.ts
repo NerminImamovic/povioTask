c6870b4c5ac74ac0d8cd34305d9c80dd91392b7c
Nermin <nimamovic9@gmail.com>
2021-10-25 12:38:23 +0200

    remove server listening for test initialization

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import * as http from 'http';
 
-import { PORT } from './constants';
+import { PORT, NODE_ENV } from './constants';
 import application from './application';
 
 const server = http.createServer(application.instance);
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on :${PORT}`);
-});
+if (NODE_ENV !== 'test') {
+  server.listen(PORT, () => {
+    console.log(`Server is listening on :${PORT}`);
+  });
+}
